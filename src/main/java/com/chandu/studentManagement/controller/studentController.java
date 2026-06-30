a37852e240b83813a94c15f091a5f186224e2ec9
chandu ravella <chandu.ravella401@gmail.com>
2026-06-30 17:03:18 +0530

    add logs

--- src/main/java/com/chandu/studentManagement/controller/studentController.java
+++ src/main/java/com/chandu/studentManagement/controller/studentController.java
@@ -3,6 +3,7 @@ package com.chandu.studentManagement.controller;
 import com.chandu.studentManagement.entity.StudentEntity;
 import com.chandu.studentManagement.model.Student;
 import com.chandu.studentManagement.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/students")
 public class studentController {
 
@@ -22,13 +24,17 @@ public class studentController {
     @GetMapping("/getAllStudents")
     public ResponseEntity<List<StudentEntity>> getAllStudents() {
 
+        log.info("Request received to get all students");
+
         List<StudentEntity> students = studentService.getAllStudents();
         return ResponseEntity.ok(students);
     }
 
     @PostMapping("/addNewStudent")
     public ResponseEntity<StudentEntity> addNewStudent(Student student) {
+        log.info("Request received to add a new student: {}", student);
         StudentEntity savedStudent = studentService.saveStudent(student);
+        log.info("Student added successfully");
         return ResponseEntity.ok(savedStudent);
     }
 }
--- src/main/java/com/chandu/studentManagement/service/StudentService.java
+++ src/main/java/com/chandu/studentManagement/service/StudentService.java
@@ -3,18 +3,21 @@ package com.chandu.studentManagement.service;
 import com.chandu.studentManagement.dao.StudentDao;
 import com.chandu.studentManagement.entity.StudentEntity;
 import com.chandu.studentManagement.model.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class StudentService {
 
     @Autowired
     private StudentDao studentDao;
 
     public List<StudentEntity> getAllStudents() {
+        log.info("Fetching all students from database");
         return studentDao.findAll();
     }
 
@@ -22,6 +25,7 @@ public class StudentService {
         StudentEntity studentEntity = new StudentEntity();
         studentEntity.setName(student.getName());
         studentEntity.setEmail(student.getEmail());
+        log.info("Saving student into database: {}", studentEntity);
         return studentDao.save(studentEntity);
     }
 }
